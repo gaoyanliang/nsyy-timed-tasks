952d1057e15766b18157804d0f7f0e822f29f165
gaoyanliang <yanliang2025@163.com>
2024-04-24 19:10:31 +0800

    add 危机值对接接口

--- gyldbmodules/critical_value_task/critical_value.py
+++ gyldbmodules/critical_value_task/critical_value.py
@@ -1,16 +1,45 @@
 import cx_Oracle
+import os
+
+# os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.AL32UTF8'
 
 
-def query_cv(cv_source):
+def query_cvl_by_source(json_data):
     try:
-        # 连接数据库
         connection = cx_Oracle.connect("system", "d67v7rbZyV", "192.168.3.240:1521/orcl")
-        # 创建游标
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM NS_EXT.PACS危急值上报表 ")
-        for row in cursor:
-            print(row)
-    except cx_Oracle.Error as error:
+        cv_source = int(json_data.get('cv_source'))
+        page_number = int(json_data.get('page_number'))
+        page_size = int(json_data.get('page_size'))
+        start_time = json_data.get('start_time')
+        end_time = json_data.get('end_time')
+
+        time_sql = ''
+        if start_time and end_time:
+            time_sql = f'AND ALERTDT BETWEEN TO_DATE(\'{start_time}\', \'YYYY_MM_DD HH24:MI:SS\')  AND TO_DATE(\'{end_time}\', \'YYYY_MM_DD HH24:MI:SS\')'
+
+        # NS_EXT.PACS危急值上报表   inter_lab_resultalert
+        cursor.execute(f"""
+            SELECT * from NS_EXT.PACS危急值上报表
+            WHERE "cv_source" = {cv_source} {time_sql}
+        """)
+        retl = cursor.fetchall()
+        total = len(retl) if retl else 0
+
+        cursor.execute(f'SELECT * from NS_EXT.PACS危急值上报表 WHERE "cv_source" = {cv_source} {time_sql} ')
+        results = cursor.fetchall()
+        start_index = (page_number - 1) * page_size
+        end_index = start_index + page_size
+        results = results[start_index:end_index]
+
+        columns = [desc[0] for desc in cursor.description]
+        results_with_columns = []
+        for row in results:
+            resultd = dict(zip(columns, row))
+            results_with_columns.append(resultd)
+
+        return results_with_columns, total
+    except Exception as error:
         # 如果出现数据库错误，则执行此处的代码块
         print("数据库错误:", error)
     finally:
@@ -30,11 +59,14 @@ def report_cv(json_data):
         cursor = connection.cursor()
 
         fields = ",".join(json_data.keys())
-        placeholders = ":" + ",:".join(json_data.keys())
-        sql = f"INSERT INTO NS_EXT.PACS危急值上报表 ({fields}) VALUES ({placeholders})"
+        fields = fields.replace('cv_source', '\"cv_source\"')
+        #placeholders = ":" + ",:".join(json_data.keys())
+        values = ', '.join([f"'{value}'" if isinstance(value, str) else str(value) for value in json_data.values()])
+        sql = f"INSERT INTO NS_EXT.PACS危急值上报表 ({fields}) VALUES ({values})"
 
+        sql = sql.replace('\''+json_data['ALERTDT']+'\'', 'TO_DATE(\'' + json_data['ALERTDT'] + '\', \'YYYY-MM-DD HH24:MI:SS\')')
         # 执行插入操作
-        cursor.execute(sql, json_data)
+        cursor.execute(sql)
         # 提交事务
         connection.commit()
     except cx_Oracle.Error as error:
@@ -57,12 +89,12 @@ def report_cv(json_data):
 def cancel_cv(cv_id, cv_source):
     try:
         # 连接数据库
-        connection = cx_Oracle.connect("system", "d67v7rbZyV", "192.168.3.240:1526/orcl")
+        connection = cx_Oracle.connect("system", "d67v7rbZyV", "192.168.3.240:1521/orcl")
         # 创建游标
         cursor = connection.cursor()
 
         # 构建更新语句
-        sql = f"UPDATE NS_EXT.PACS危急值上报表 SET VALIDFLAG = 0 WHERE RESULTALERTID = {cv_id} and cv_source = {cv_source}"
+        sql = f"UPDATE NS_EXT.PACS危急值上报表 SET VALIDFLAG = 0 WHERE RESULTALERTID = \'{cv_id}\' and \"cv_source\" = {cv_source}"
 
         # 执行插入操作
         cursor.execute(sql)
--- gyldbmodules/critical_value_task/critical_value_router.py
+++ gyldbmodules/critical_value_task/critical_value_router.py
@@ -9,16 +9,28 @@ from gyldbmodules.critical_value_task import critical_value
 cv = Blueprint('critical value', __name__, url_prefix='/cv')
 
 
-@cv.route('/query_cv', methods=['GET', 'POST'])
-def query_cv():
+@cv.route('/query_cvl_by_source', methods=['GET', 'POST'])
+def query_cvl_by_source():
     try:
         json_data = json.loads(request.get_data().decode('utf-8'))
-        critical_value.query_cv(int(json_data.get('cv_source')))
-    except Exception as e:
+        cv_source = json_data.get('cv_source')
+        page_number = json_data.get('page_number')
+        page_size = json_data.get('page_size')
+        if not cv_source or not page_size or not page_number:
+            return jsonify({
+                'code': 50000,
+                'res': 'cv_source, page_size, page_number can not be nil'
+            })
+        cvl, total = critical_value.query_cvl_by_source(json_data)
+    except Exception:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(f"[{timestamp}] Exception occurred: {traceback.print_exc()}")
     return jsonify({
-        'code': 20000
+        'code': 20000,
+        'data': {
+            'cvl': cvl,
+            'total': total
+        }
     })
 
 
@@ -27,9 +39,14 @@ def report_cv():
     try:
         json_data = json.loads(request.get_data().decode('utf-8'))
         critical_value.report_cv(json_data)
-    except Exception as e:
+    except Exception:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"[{timestamp}] Exception occurred: {traceback.print_exc()}")
+        msg = f"[{timestamp}] Exception occurred: {traceback.print_exc()}"
+        print(msg)
+        return jsonify({
+            'code': 50000,
+            'res': msg
+        })
     return jsonify({
         'code': 20000
     })
@@ -40,9 +57,14 @@ def cancel_cv():
     try:
         json_data = json.loads(request.get_data().decode('utf-8'))
         critical_value.cancel_cv(json_data.get('cv_id'), int(json_data.get('cv_source')))
-    except Exception as e:
+    except Exception:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"[{timestamp}] Exception occurred: {traceback.print_exc()}")
+        msg = f"[{timestamp}] Exception occurred: {traceback.print_exc()}"
+        print(msg)
+        return jsonify({
+            'code': 50000,
+            'res': msg
+        })
     return jsonify({
         'code': 20000
     })
